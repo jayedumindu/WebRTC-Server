4fcb1817e83a94a4d8f8b3aa0d0dc98dbd516d2f
jayedumindu <jayedumindu@gamil.com>
2023-05-07 13:17:03 +0530

    configuration added for build

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = {
+  target: "node",
+  entry: "./index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx"],
+    fallback: {
+      path: false,
+      buffer: false,
+      crypto: false,
+      url: false,
+      bufferutil: false,
+      "utf-8-validate": false,
+      util: false,
+      fs: false,
+      string_decoder: false,
+      querystring: false,
+      http: false,
+      net: false,
+      os: false,
+      zlib: false,
+      stream: false,
+    },
+  },
+};
