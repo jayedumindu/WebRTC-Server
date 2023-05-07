271302a6a459a37a076aa557a43c52529f02828b
jayedumindu <jayedumindu@gamil.com>
2023-05-07 23:47:09 +0530

    updated

--- webpack.config.js
+++ webpack.config.js
@@ -5,33 +5,37 @@ const __filename = fileURLToPath(import.meta.url);
 
 const __dirname = path.dirname(__filename);
 
-export const target = "node";
-export const entry = "./src/index.cjs";
-export const externals = {
-  peer: "require('peer')",
-};
-export const output = {
-  filename: "main.js",
-  path: path.resolve(__dirname, "dist"),
-};
-export const mode = "development";
-export const resolve = {
-  extensions: [".js", ".jsx"],
-  fallback: {
-    path: false,
-    buffer: false,
-    crypto: false,
-    url: false,
-    bufferutil: false,
-    "utf-8-validate": false,
-    util: false,
-    fs: false,
-    string_decoder: false,
-    querystring: false,
-    http: false,
-    net: false,
-    os: false,
-    zlib: false,
-    stream: false,
+export default {
+  target: "node",
+  entry: "./src/index.js",
+  externals: {
+    peer: "require('peer')",
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx"],
+    fallback: {
+      path: false,
+      buffer: false,
+      crypto: false,
+      url: false,
+      bufferutil: false,
+      "utf-8-validate": false,
+      util: false,
+      fs: false,
+      string_decoder: false,
+      querystring: false,
+      http: false,
+      net: false,
+      os: false,
+      zlib: false,
+      stream: false,
+    },
   },
 };
+
+
