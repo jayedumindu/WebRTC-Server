df599cbcfe49842ffa688f7407f0b939c427abd8
jayedumindu <jayedumindu@gamil.com>
2023-05-07 23:03:17 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-var PeerServer = require("peer").PeerServer;
+import { PeerServer } from "peer";
+// var PeerServer = require("peer").PeerServer;
 var server = PeerServer({ port: 3500, path: "/connect" });
 console.log("server connected!!")
