ef8c73788e3750f7269ea942ba67a5e5a79c4cf7
jayedumindu <jayedumindu@gamil.com>
2023-05-08 19:12:13 +0530

    changes for the configuration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import { PeerServer } from "peer";
-// var PeerServer = require("peer").PeerServer;
+import { ip } from 'ip'
 var server = PeerServer({ port: 443, path: "/connect" });
-console.log("server connected!!")
+
+server.on("connection", function (id) {
+  console.log("new connection with id " + id);
+});
+
+server.on("disconnect", function (id) {
+  console.log("disconnect with id " + id);
+});
+
+console.log("peer server running on " + ip.address() + ":" + port);
